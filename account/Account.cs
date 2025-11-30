a73634e066001836101a99baf05319333d8fb5dc
sunfloware <aegeansunflower@gmail.com>
2025-11-30 18:43:47 +0300

    Add Account class with name getter and setter

---
+++ account/Account.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Account
+{
+    private string name;
+    public void SetName(string accountName)
+    {
+        name = accountName;
+    }
+    public string GetName()
+    {
+        return name;
+    }
+}
